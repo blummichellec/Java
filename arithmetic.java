265223ae8c64420472db5d42caa2185a9fdea0e4
Michelle Blum <blummichellec@yahoo.com>
2017-07-14 20:06:49 -0400

    Add files via upload

---
+++ arithmetic.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class calcc {
+
+	public static void main(String[] args) {
+		int number;
+		int number2;
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("Pick a number.");
+		number = keyboard.nextInt();
+		System.out.println("Pick another number.");
+		number2 = keyboard.nextInt();
+		int sum = number + number2;
+		int difference = number - number2;
+		int average = (number + number2) / 2;
+		System.out.println("Sum: " + sum);
+		System.out.println("Difference: " + difference);
+		System.out.println("Average: " + average);
+		System.out.print("Absolute value: " + Math.abs(number) + ", ");
+		System.out.println(Math.abs(number2));
+		System.out.print("To the fourth power: " + Math.pow(number, 4) + ", ");
+		System.out.println(Math.pow(number2, 4));
+		System.out.print("Square rooted: " + Math.sqrt(number) + ", ");
+		System.out.println(Math.sqrt(number2));
+		System.out.println("Maximum value: " + Math.max(number,  number2));
+		System.out.println("Minimum value: " + Math.min(number, number2));
+		System.out.println("Increment of 1: " + ++number);
+		System.out.println("Decrement of 1: " + --number2);
+	
+
+	}
+
+}
